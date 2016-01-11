ea7614ffab4672cc8d237dbe26bb7c08a6301efd
Pankaj Vohra <pjvohra@gmail.com>
2016-01-10 20:50:07 -0500

    first

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Pankaj Vohra on 1/10/16.
+//  Copyright © 2016 Pankaj Vohra. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Pankaj Vohra on 1/9/16.
+//  Copyright © 2016 Pankaj Vohra. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
